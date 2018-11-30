eab6c302081e855d0c6104b4b10a9f96b243d386
triplewy <triplewy@gmail.com>
2018-11-29 19:26:37 -0500

    Parses month for csv now

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -336,8 +336,21 @@ module.exports = function(conn, loggedIn, csvUpload) {
     function parseDate(date, shift) {
       const dateArr1 = date.split(' ')
       const dateArr2 = dateArr1[1].split('/')
-
-      var output = '20' + dateArr2[1] + '-10-' + dateArr1[0]
+      const months = {
+        'Jan' : 1,
+        'Feb' : 2,
+        'Mar' : 3,
+        'Apr' : 4,
+        'May' : 5,
+        'Jun' : 6,
+        'Jul' : 7,
+        'Aug' : 8,
+        'Sep' : 9,
+        'Oct' : 10,
+        'Nov' : 11,
+        'Dec' : 12
+      }
+      var output = '20' + dateArr2[1] + '-' + months[dateArr2[0]] + '-' + dateArr1[0]
       if (shift == 'Day') {
         output += 'T10:00'
       } else {
--- server/server.js
+++ server/server.js
@@ -107,30 +107,28 @@ function handleDisconnect() {
   });                                     // process asynchronous requests in the meantime
 }
 
-
-
-conn.query('SET foreign_key_checks = 0')
-conn.query('DROP TABLE IF EXISTS downtimeImages')
-conn.query('DROP TABLE IF EXISTS downtime')
-conn.query('DROP TABLE IF EXISTS machines')
-conn.query('DROP TABLE IF EXISTS assemblyLineUsers')
-conn.query('DROP TABLE IF EXISTS assemblyLines')
-conn.query('DROP TABLE IF EXISTS logins')
-conn.query('DROP TABLE IF EXISTS users')
-conn.query('DROP TABLE IF EXISTS companies')
-conn.query('SET foreign_key_checks = 1')
-
-conn.query('CREATE TABLE IF NOT EXISTS companies (companyId INTEGER AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL UNIQUE, createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)')
-conn.query('CREATE TABLE IF NOT EXISTS users (userId INTEGER AUTO_INCREMENT PRIMARY KEY, companyId INTEGER, createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY(companyId) REFERENCES companies(companyId))')
-conn.query('CREATE TABLE IF NOT EXISTS logins (loginId INTEGER AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL, userId INTEGER NOT NULL, passwordHash CHAR(60), FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE)')
-conn.query('CREATE TABLE IF NOT EXISTS assemblyLines (lineId INTEGER AUTO_INCREMENT PRIMARY KEY, companyId INTEGER NOT NULL, name VARCHAR(255) NOT NULL, availableMin INTEGER NOT NULL, morningShift INTEGER NOT NULL, eveningShift INTEGER NOT NULL, createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY (companyId) REFERENCES companies(companyId))')
-conn.query('CREATE TABLE IF NOT EXISTS assemblyLineUsers (lineId INTEGER NOT NULL, userId INTEGER NOT NULL, FOREIGN KEY(lineId) REFERENCES assemblyLines(lineId), FOREIGN KEY(userId) REFERENCES users(userId) ON DELETE CASCADE, UNIQUE(lineId, userId))')
-conn.query('CREATE TABLE IF NOT EXISTS machines (machineId INTEGER AUTO_INCREMENT PRIMARY KEY, lineId INTEGER NOT NULL, name VARCHAR(255) NOT NULL, icon_url VARCHAR(255) NOT NULL, createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,  FOREIGN KEY(lineId) REFERENCES assemblyLines(lineId), UNIQUE(lineId, name))')
-conn.query('CREATE TABLE IF NOT EXISTS downtime (downtimeId INTEGER AUTO_INCREMENT PRIMARY KEY, machineId INTEGER NOT NULL, lineId INTEGER NOT NULL, lineLeaderName VARCHAR(255) NOT NULL, downtime INTEGER NOT NULL, description TEXT, createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY(machineId) REFERENCES machines(machineId), FOREIGN KEY(lineId) REFERENCES assemblyLines(lineId))')
-conn.query('CREATE TABLE IF NOT EXISTS downtimeImages (downtimeImageId INTEGER AUTO_INCREMENT PRIMARY KEY, downtimeId INTEGER NOT NULL, imageUrl VARCHAR(255) NOT NULL, createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY(downtimeId) REFERENCES downtime(downtimeId))')
-
-conn.query('CREATE TRIGGER before_downtime_insert BEFORE INSERT ON downtime FOR EACH ROW BEGIN ' +
-'SET NEW.lineId = (SELECT lineId FROM machines WHERE machineId = NEW.machineId); END;')
+// conn.query('SET foreign_key_checks = 0')
+// conn.query('DROP TABLE IF EXISTS downtimeImages')
+// conn.query('DROP TABLE IF EXISTS downtime')
+// conn.query('DROP TABLE IF EXISTS machines')
+// conn.query('DROP TABLE IF EXISTS assemblyLineUsers')
+// conn.query('DROP TABLE IF EXISTS assemblyLines')
+// conn.query('DROP TABLE IF EXISTS logins')
+// conn.query('DROP TABLE IF EXISTS users')
+// conn.query('DROP TABLE IF EXISTS companies')
+// conn.query('SET foreign_key_checks = 1')
+//
+// conn.query('CREATE TABLE IF NOT EXISTS companies (companyId INTEGER AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL UNIQUE, createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)')
+// conn.query('CREATE TABLE IF NOT EXISTS users (userId INTEGER AUTO_INCREMENT PRIMARY KEY, companyId INTEGER, createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY(companyId) REFERENCES companies(companyId))')
+// conn.query('CREATE TABLE IF NOT EXISTS logins (loginId INTEGER AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL, userId INTEGER NOT NULL, passwordHash CHAR(60), FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE)')
+// conn.query('CREATE TABLE IF NOT EXISTS assemblyLines (lineId INTEGER AUTO_INCREMENT PRIMARY KEY, companyId INTEGER NOT NULL, name VARCHAR(255) NOT NULL, availableMin INTEGER NOT NULL, morningShift INTEGER NOT NULL, eveningShift INTEGER NOT NULL, createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY (companyId) REFERENCES companies(companyId))')
+// conn.query('CREATE TABLE IF NOT EXISTS assemblyLineUsers (lineId INTEGER NOT NULL, userId INTEGER NOT NULL, FOREIGN KEY(lineId) REFERENCES assemblyLines(lineId), FOREIGN KEY(userId) REFERENCES users(userId) ON DELETE CASCADE, UNIQUE(lineId, userId))')
+// conn.query('CREATE TABLE IF NOT EXISTS machines (machineId INTEGER AUTO_INCREMENT PRIMARY KEY, lineId INTEGER NOT NULL, name VARCHAR(255) NOT NULL, icon_url VARCHAR(255) NOT NULL, createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,  FOREIGN KEY(lineId) REFERENCES assemblyLines(lineId), UNIQUE(lineId, name))')
+// conn.query('CREATE TABLE IF NOT EXISTS downtime (downtimeId INTEGER AUTO_INCREMENT PRIMARY KEY, machineId INTEGER NOT NULL, lineId INTEGER NOT NULL, lineLeaderName VARCHAR(255) NOT NULL, downtime INTEGER NOT NULL, description TEXT, createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY(machineId) REFERENCES machines(machineId), FOREIGN KEY(lineId) REFERENCES assemblyLines(lineId))')
+// conn.query('CREATE TABLE IF NOT EXISTS downtimeImages (downtimeImageId INTEGER AUTO_INCREMENT PRIMARY KEY, downtimeId INTEGER NOT NULL, imageUrl VARCHAR(255) NOT NULL, createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY(downtimeId) REFERENCES downtime(downtimeId))')
+//
+// conn.query('CREATE TRIGGER before_downtime_insert BEFORE INSERT ON downtime FOR EACH ROW BEGIN ' +
+// 'SET NEW.lineId = (SELECT lineId FROM machines WHERE machineId = NEW.machineId); END;')
 
 var upload = multer({
   storage: multerS3({
