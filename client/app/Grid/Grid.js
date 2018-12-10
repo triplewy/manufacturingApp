287131713e6e2367c7b6d7fdbc97b2ec6ddc6184
triplewy <triplewy@gmail.com>
2018-12-09 20:45:21 -0500

    forgot to delete commented grid stuff

--- client/app/Grid/Grid.js
+++ client/app/Grid/Grid.js
@@ -1,83 +1,9 @@
 import React from 'react';
 import { ScrollView, View, RefreshControl, FlatList, StyleSheet, Text, Dimensions, TouchableOpacity, Platform } from 'react-native';
 import { fetchGrid, fetchLines, setLine } from './grid.operations'
-// import { fetchLines } from '../fetchLines.js'
 import { connect } from 'react-redux'
 import GridItem from './GridItem'
 import ChooseModal from '../ChooseModal'
-
-// export default class Grid extends React.Component {
-//   constructor(props) {
-//     super(props);
-//
-//     this.state = {
-//       grid: [],
-//       lines: [],
-//       line: 0
-//     };
-//
-//     this.fetchGrid = this.fetchGrid.bind(this)
-//     this.setLine = this.setLine.bind(this)
-//     this.renderItem = this.renderItem.bind(this)
-//   }
-//
-//   componentDidMount() {
-//     fetchLines().then(data => {
-//       this.setState({lines: data}, () => {
-//         this.fetchGrid()
-//       })
-//     })
-//   }
-//
-//   fetchGrid() {
-//     fetch(global.API_URL + '/api/grid/line/' + this.state.lines[this.state.line].lineId, {credentials: 'include'})
-//     .then(res => res.json())
-//     .then(data => {
-//       this.setState({grid: data})
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     })
-//   }
-//
-//   setLine(index) {
-//     this.setState({line: index}, () => {
-//       this.fetchGrid()
-//     })
-//   }
-//
-//   renderItem(item) {
-//     return (
-//       <GridItem {...item.item} navigation={this.props.navigation} />
-//     )
-//   }
-//
-//   render() {
-//     var columns = 4
-//     if (!Platform.isPad) {
-//       columns = 3
-//     }
-//     return (
-//       <ScrollView>
-//         <ChooseModal
-//           items={this.state.lines}
-//           index={this.state.line}
-//           selectItem={this.setLine}
-//         />
-//         <View style={styles.statsView}>
-//           <FlatList
-//             data={this.state.grid}
-//             renderItem={this.renderItem}
-//             keyExtractor={(item, index) => index.toString()}
-//             numColumns={columns}
-//             contentContainerStyle={{alignItems: 'center'}}
-//           />
-//         </View>
-//       </ScrollView>
-//     )
-//   }
-// }
-
 
 class Grid extends React.Component {
 
